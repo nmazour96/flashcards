95254c66f3f921121fbd8d211e5676db22b4cd57
nmazour96 <nmazour@nebrwesleyan.edu>
2014-11-04 14:32:11 -0500

    Remade the first question

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
+document.getElementById('title');
+'title'.style.fontSize = '75px';
+'title'.style.textAlign = 'center';
+'title'.style.color = '#00F';
+
+document.getElementById('basicQuestionsTitle');
+'basicQuestionsTitle'.style.textAlign = 'center';
+'basicQuestionsTitle'.style.fontSize = '35px';
+'basicQuestionsTitle'.style.color = '#F00';
+
 function correct() {
-  document.getElementById('response').innerHTML = 'Correct';
+  document.getElementById('responseOne').innerHTML = 'Correct';
 }
 
 function incorrect() {
-  document.getElementById('response').innerHTML = 'Incorrect';
+  document.getElementById('responseOne').innerHTML = 'Incorrect';
 }
 
-document.getElementById('4').addEventListener('click',correct);
-document.getElementById('5').addEventListener('click',incorrect);
-document.getElementById('7').addEventListener('click',incorrect);
-document.getElementById('3').addEventListener('click',incorrect);
+document.getElementById('four').addEventListener('click',correct);
+document.getElementById('five').addEventListener('click',incorrect);
+document.getElementById('seven').addEventListener('click',incorrect);
+document.getElementById('three').addEventListener('click',incorrect);

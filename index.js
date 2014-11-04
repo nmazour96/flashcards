fe1d5a3e111fe6babd7ca50623abe4a7f4babb32
nmazour96 <nmazour@nebrwesleyan.edu>
2014-11-04 10:52:14 -0500

    Added First Basic Question

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
+function correct() {
+  document.getElementById('response').innerHTML = 'Correct';
+}
+
+function incorrect() {
+  document.getElementById('response').innerHTML = 'Incorrect';
+}
+
+document.getElementById('4').addEventListener('click',correct);
+document.getElementById('5').addEventListener('click',incorrect);
+document.getElementById('7').addEventListener('click',incorrect);
+document.getElementById('3').addEventListener('click',incorrect);

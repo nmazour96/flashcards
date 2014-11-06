3831583323cf83eb9af2dbaee3421b3df3ffda45
nmazour96 <nmazour@nebrwesleyan.edu>
2014-11-06 09:41:32 -0500

    Fixed variable errors

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
-document.getElementById(title);
+var title = document.getElementById('title');
 title.style.fontSize = '75px';
 title.style.textAlign = 'center';
 title.style.color = '#00F';
 
-document.getElementById(questionSectionTitle);
+var questionSectionTitle = document.getElementById(questionSectionTitle);
 questionSectionTitle.style.textAlign = 'center';
 questionSectionTitle.style.fontSize = '50px';
 questionSectionTitle.style.color = '#F00';
 
-document.getElementById(question);
+var question = document.getElementById(question);
 question.style.fontSize = '35px';
 question.style.color = '#0F0';
 
+var response = document.getElementById(response);
+response.style.fontSize = '35px';
+
 function correct() {
-  document.getElementById('responseOne').innerHTML = 'Correct';
+  document.getElementById('response').innerHTML = 'Correct';
 }
 
 function incorrect() {
-  document.getElementById('responseOne').innerHTML = 'Try Again';
+  document.getElementById('response').innerHTML = 'Try Again';
 }
 
 document.getElementById('four').addEventListener('click',correct);

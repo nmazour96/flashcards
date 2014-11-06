255fcbff7da70e5e5d6286359b3cd2be821c81bb
nmazour96 <nmazour@nebrwesleyan.edu>
2014-11-06 15:22:06 -0500

    fasnlf

--- index.js
+++ index.js
@@ -20,9 +20,16 @@ function basic_question() {
   currentBasic = currentBasic + 1;
 }
 
-document.getElementById('optionOne').innerHTML = basicQuestions[currentBasic].options[0];
-document.getElementById('optionTwo').innerHTML = basicQuestions[currentBasic].options[1];
-document.getElementById('optionThree').innerHTML = basicQuestions[currentBasic].options[2];
-document.getElementById('optionFour').innerHTML = basicQuestions[currentBasic].options[3];
+document.getElementById('optionOne').innerHTML = basicQuestions[0].options[0];
+document.getElementById('optionTwo').innerHTML = basicQuestions[0].options[1];
+document.getElementById('optionThree').innerHTML = basicQuestions[0].options[2];
+document.getElementById('optionFour').innerHTML = basicQuestions[0].options[3];
 
 document.getElementById('optionTwo').addEventListener('click', basic_question);
+
+document.getElementById('optionOne').innerHTML = basicQuestions[1].options[0];
+document.getElementById('optionTwo').innerHTML = basicQuestions[1].options[1];
+document.getElementById('optionThree').innerHTML = basicQuestions[1].options[2];
+document.getElementById('optionFour').innerHTML = basicQuestions[1].options[3];
+
+document.getElementById('optionThree').addEventListener('click', basic_question);

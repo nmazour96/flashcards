d87c936b29c960c062771f2a67776f8d7cde3d6a
nmazour96 <nmazour@nebrwesleyan.edu>
2014-11-18 12:01:28 -0500

    changed choices to a list

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 var question = document.getElementById('question');
 var correct = document.getElementById('correct');
-var choices = [document.getElementById('optionOne'), document.getElementById('optionTwo'),document.getElementById('optionThree'), document.getElementById('optionFour')];
+var choices = [document.getElementById('options')];
 
 var score = 0;
 var currentBasic = 0;

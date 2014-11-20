68f42b7b45cbb76eac3b6a2188ee251147e7ba66
nmazour96 <nmazour@nebrwesleyan.edu>
2014-11-20 10:26:44 -0500

    fixed link

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ congrats.style.display = 'none';
 
 var reset = document.getElementById('reset');
 reset.style.display = 'none';
-reset.innerHTML = 'RESET';
 
 var lastQuestionOptions = document.getElementById('lastQuestionOptions');
 lastQuestionOptions.style.display = 'none';
